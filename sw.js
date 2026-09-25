9b822ccd859edf37998228ecdd1281ab87e18340
malahim05-arch <mal.ahim05@gmail.com>
2026-09-26 01:25:31 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="wf-v06";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
